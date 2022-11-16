66bb79cbeddadefa478d03e2384aaf164de22483
Tim La <tlamarketing111@gmail.com>
2022-11-16 05:45:49 -0500

    creating HOC to wrap other components

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const PageHOC = () => {
+import { logo, heroImg } from '../assets'
+import styles from '../styles'
+
+const PageHOC = (Component, title, description) => () => {
+    const navigate = useNavigate();
+
     return (
         <div>PageHOC</div>
     )
