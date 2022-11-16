4b60e6e1312cc347f7be3caf83e41739a619873d
Tim La <tlamarketing111@gmail.com>
2022-11-16 07:31:12 -0500

    created function to update wallet address

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -6,6 +6,17 @@ import { useNavigate } from 'react-router-dom'
 const GlobalContext = createContext()
 
 export const GlobalContextProvider = ({ children }) => {
+    const [walletAddress, setWalletAddress] = useState("")
+
+    const updateCurrentWalletAddress = async () => {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts"
+        })
+    }
+
+    useEffect(() => {
+        updateCurrentWalletAddress()
+    }, [])
     return (
         <GlobalContext.Provider value={{
             demo: "test"
