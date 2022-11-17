8d97ecf5bf4122ecae75d1853e965fb66af92eef
Tim La <tlamarketing111@gmail.com>
2022-11-17 07:01:05 -0500

    created new file in context folder

---
+++ client/src/context/createEventListeners.js
@@ -0,0 +1,12 @@
+import { ethers } from 'ethers'
+import { ABI } from '../contract'
+
+const AddNewEvent = (eventFilter, provider, cb) => {
+    provider.removeListener(eventFilter);
+
+    provider.on(eventFilter, (logs) => {
+        const parsedLog = (new ethers.utils.Interface(ABI)).parseLog(Logs)
+    })
+
+    cb(parsedLog)
+}
