b184ebe4f1aeb42772fe6ca6a92a4a51e4511611
Tim La <tlamarketing111@gmail.com>
2022-11-18 11:58:53 -0500

    creating layout for gameload page

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CustomButton } from './CustomButton'
+import { useGlobalContext } from '../context'
+import { player01, player02 } from '../assets'
+import styles from '../styles'
 
 const GameLoad = () => {
+    const { walletAddress } = useGlobalContext();
+    const navigate = useNavigate();
+
     return (
-        <div>GameLoad</div>
+        <div className={`${styles.flexBetween} ${styles.gameLoadContainer}`}>
+            <div className={styles.gameLoadBtnBox}>
+                <CustomButton
+                    title="Choose Battleground"
+                    handleClick={() => navigate("/battleground")}
+                    restStyles="mt-6"
+                />
+            </div>
+
+            <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+                <h1 className={`${styles.headText} text-center`}>
+                    Waiting for a <br /> worthy opponent...
+                </h1>
+                <p className={styles.gameLoadText}>
+                    Protip: while you're waiting, choose your preferred battleground
+                </p>
+            </div>
+        </div>
     )
 }
 
